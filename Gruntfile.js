b120d4ab91b3f5a601d9132db63b99020c405824
Daniel Eden <dan.eden@me.com>
2013-11-11 09:01:32 -0800

    Minor workflow and CSS changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,80 @@
 module.exports = function(grunt) {
-	pkg: grunt.file.readJSON('package.json'),
-	grunt.initConfig({
-		// Sass
-		sass: {
-		  dist: {
-		  	options: {
-		  		sourcemap: true,
-		  		compass: true
-		  	},
-		  	files: {
-		  		'assets/css/style.css' : 'assets/scss/style.scss'
-		  	}
-		  }
-		},
-		// Auto-prefix CSS properties using Can I Use?
-	  autoprefixer: {
-	    options: {
-	    	// Last 2 versions of all browsers, plus IE7/8, BB10 (LOL), and Android 3+
-	    	browsers: ['last 2 versions', 'ie 8', 'ie 7', 'bb 10', 'android 3']
-	    },
-	    no_dest: {
-	    	// File to output
-	      src: 'assets/css/style.css'
-	    },
-	  },
-	  csso: {
+  pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+
+    // Sass
+    sass: {
+      dist: {
+        options: {
+          sourcemap: true,
+          compass: true
+        },
+        files: {
+          'assets/css/style.css' : 'assets/scss/style.scss'
+        }
+      }
+    },
+
+    // Auto-prefix CSS properties using Can I Use?
+    autoprefixer: {
+      options: {
+        // Last 2 versions of all browsers, plus IE7/8, BB10 (LOL), and Android 3+
+        browsers: ['last 2 versions', 'ie 8', 'ie 7', 'bb 10', 'android 3']
+      },
+      no_dest: {
+        // File to output
+        src: 'assets/css/style.css'
+      },
+    },
+
+    // Minify CSS
+    csso: {
       dist: {
         files: {
-        	// Output compressed CSS to style.min.css
+          // Output compressed CSS to style.min.css
           'assets/css/style.min.css': ['assets/css/style.css']
         }
       }
     },
+
+    // Jekyll with drafts
     jekyll: {
-    	dist: {
-    		options: {
-    			drafts: true
-    		}
-    	}
+      dist: {
+        options: {
+          drafts: true
+        }
+      }
     },
+
     // Watch files for changes
     watch: {
       css: {
-        files: ['assets/scss/*', 'assets/images/*', '!**/_site/**'],
+        files: [
+          'assets/scss/*',
+          'assets/images/*',
+          '!**/_site/**'
+        ],
         // Run Sass, autoprefixer, and CSSO
         tasks: ['sass', 'autoprefixer', 'csso'],
       },
       site: {
-      	files: ['**/*', '!**/node_modules/**', '!**/_site/**', '.htaccess'],
-      	tasks: ['jekyll'],
+        files: [
+          '**/*',
+          '.htaccess',
+          '!**/node_modules/**',
+          '!**/_site/**'
+        ],
+        tasks: ['jekyll']
       }
     }
-	});
 
-	// Register our tasks
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-csso');
-	grunt.loadNpmTasks('grunt-jekyll');
-	grunt.registerTask('default', ['watch']);
+  });
+
+  // Register our tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.registerTask('default', ['watch']);
 };
