3448a44633b8d6c89721a8e92cb51aca7ddf226a
Daniel Eden <dan.eden@me.com>
2014-01-02 17:21:18 -0800

    Update site map generator to https

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -39,7 +39,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://daneden.me"
+  MY_URL = "https://daneden.me"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
