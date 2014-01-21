c8bae1643ac233b7e2cc7cc6ae20088d7e294261
Daniel Eden <dan.eden@me.com>
2014-01-20 17:39:03 -0800

    Remove trailing slash from GH pages link, remove old Animate.css pages

--- animate/slider/fallback.js
+++
@@ -1,9 +0,0 @@
-function slide(n) {
-	$('.active').removeClass('active').addClass('inactive');
-	$('.slide' + n).removeClass('inactive').addClass('active');
-}
-
-$(document).ready(function() {
-	$('.inactive').hide();
-	$('.method').append('Using jQuery fallback');
-});
