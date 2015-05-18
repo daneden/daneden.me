2f342c6c66102244f5e8a65f1526ef01464e559c
Daniel Eden <dan.eden@me.com>
2015-05-18 10:07:02 -0700

    Update to full character set, fix CSS quirks, add autoprefixer

--- Gemfile
+++ Gemfile
@@ -6,10 +6,10 @@ group :jekyll_plugins do
   gem "jekyll-sitemap"
 end
 
-gem "autoprefixer-rails"
 gem "facets"
 gem "jekyll-assets", :git => 'https://github.com/daneden/jekyll-assets.git'
 gem "kramdown"
+gem "octopress-autoprefixer"
 gem "sass"
 gem "typogruby"
 gem "uglifier"
--- _assets/js/myfonts.js
+++ _assets/js/myfonts.js
@@ -1,6 +1,6 @@
 /*
 
- MyFonts Webfont Build ID 3027330, 2015-05-18T00:14:57-0400
+ MyFonts Webfont Build ID 3027756, 2015-05-18T12:53:09-0400
 
  The fonts listed in this notice are subject to the End User License
  Agreement(s) entered into by the website owner. All other parties are
@@ -21,18 +21,18 @@
  URL: http://www.myfonts.com/fonts/linotype/neue-haas-unica/regular/
 
 
- License: http://www.myfonts.com/viewlicense?type=web&buildid=3027330
+ License: http://www.myfonts.com/viewlicense?type=web&buildid=3027756
  Licensed pageviews: 250,000
  Webfonts copyright: Copyright &#x00A9; 2015 Monotype Imaging Inc.  All rights reserved.
 
  ? 2015 MyFonts Inc
 */
-var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/2e3182";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;
+var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/2e332c";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;
 if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
 var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:36,type:"woff2"},
 {version:6,type:"woff"},{version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozilla.*(iPhone|iPad|BlackBerry).*AppleWebKit.*Safari",
 versionRegex:"1.0",type:[{version:1,type:"svg#wf"}]},{regex:"Version/(\\d+\\.\\d+)(\\.\\d+)? Safari/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:5.1,type:"woff"},{version:3.1,type:"ttf"}]},{regex:"Opera/(\\d+\\.\\d+)(.+)Version/(\\d+\\.\\d+)(\\.\\d+)?",versionRegex:"new Number(RegExp.$3)",type:[{version:24,type:"woff2"},{version:11.1,type:"woff"},{version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
 a:for(;i<browLen;i++){var regex=RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(!0==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("woff2"!=webfontType||woff2Enabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
 /(Macintosh|Android)/.test(navigator.userAgent)&&"svg#wf"!=webfontType&&(suffix="_unhinted");var head=document.getElementsByTagName("head")[0],stylesheet=document.createElement("style");stylesheet.setAttribute("type","text/css");head.appendChild(stylesheet);
-for(var fonts=[{fontFamily:"NeueHaasUnica",fontWeight:"bold",fontStyle:"normal",url:wfpath+"2E3182_0"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"bold",fontStyle:"italic",url:wfpath+"2E3182_1"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"normal",fontStyle:"italic",url:wfpath+"2E3182_2"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"normal",fontStyle:"normal",url:wfpath+"2E3182_3"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<
+for(var fonts=[{fontFamily:"NeueHaasUnica",fontWeight:"bold",fontStyle:"normal",url:wfpath+"2E332C_0"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"bold",fontStyle:"italic",url:wfpath+"2E332C_1"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"normal",fontStyle:"italic",url:wfpath+"2E332C_2"+suffix+"_0."+webfontType},{fontFamily:"NeueHaasUnica",fontWeight:"normal",fontStyle:"normal",url:wfpath+"2E332C_3"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<
 len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
