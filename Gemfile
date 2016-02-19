7124e5212c961289c8174d07c6e6d7c7fc8c4ab2
daneden <dte@dropbox.com>
2016-02-19 12:40:51 -0800

    Begin redesign and migration to Gulp

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem "jekyll"
+gem "jekyll", "~>3.0.3"
 
 group :jekyll_plugins do
   gem "jekyll-sitemap"
@@ -9,7 +9,7 @@ end
 gem "autoprefixer-rails"
 gem "coffee-script"
 gem "facets"
-gem "jekyll-assets"
+gem "jekyll-assets", "~>2.1.0"
 gem "kramdown"
 gem "sass"
 gem "typogruby"
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp         = require('gulp');
+var postcss      = require('gulp-postcss');
+var cssnext      = require('postcss-cssnext');
+var nano         = require('gulp-cssnano');
+var shell        = require('gulp-shell');
+var autoprefixer = require('gulp-autoprefixer');
+var atImport     = require('postcss-import');
+var cp           = require('child_process');
+var browserSync  = require('browser-sync').create();
+
+var processors = [
+  atImport,
+  cssnext({
+    'autoprefixer': ['last 2 version'],
+    'customProperties': {
+      preserve: true
+    },
+    'colorFunction': true,
+    'customSelectors': true,
+    'sourcemap': true
+  })
+];
+
+gulp.task('styles', function() {
+  return gulp.src('./_assets/src/style.css')
+  .pipe(postcss(processors))
+  .pipe(nano({discardComments: {removeAll: true}}))
+  .pipe(gulp.dest('./_assets/css'));
+});
+
+gulp.task('jekyll-build', shell.task(['bundle exec jekyll build --watch']));
+
+gulp.task('jekyll-serve', function() {
+  browserSync.init({ server: { baseDir: '_site/' } });
+  gulp.watch('./_assets/src/**/*.css', ['styles']);
+  gulp.watch('./_site/**/*.*').on('change', browserSync.reload);
+  gulp.watch('./_assets/css/style.css').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['jekyll-build', 'jekyll-serve', 'styles']);
