9826e1c9b4481fe9d3338d441a1a478e1e659625
daneden <dte@dropbox.com>
2016-03-10 16:25:59 -0800

    Add minifying and correct og meta tags

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp         = require('gulp');
+var util         = require('util');
 var postcss      = require('gulp-postcss');
 var cssnext      = require('postcss-cssnext');
 var nano         = require('gulp-cssnano');
@@ -7,6 +8,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var atImport     = require('postcss-import');
 var cp           = require('child_process');
 var browserSync  = require('browser-sync').create();
+var htmlmin      = require('gulp-html-minifier');
 
 var processors = [
   atImport,
@@ -32,8 +34,21 @@ gulp.task('styles', function() {
   .pipe(gulp.dest('./_assets/css'));
 });
 
+gulp.task('buildSite', shell.task('bundle exec jekyll build --incremental'));
+
+gulp.task('minify', ['buildSite'], function() {
+  return gulp.src('_site/**/*.html')
+    .pipe(htmlmin({
+      collapseWhiteSpace: true,
+      removeTagWhitespace: true,
+      lint: true,
+      minifyJS: true
+      }))
+    .pipe(gulp.dest('_site'));
+});
+
 gulp.task('jekyll-build', shell.task(['bundle exec jekyll build --incremental --watch']));
-gulp.task('jekyll-build-once', shell.task(['bundle exec jekyll build --incremental']));
+gulp.task('jekyll-build-once', ['buildSite', 'minify']);
 
 gulp.task('jekyll-serve', function() {
   browserSync.init({ server: { baseDir: '_site/' } });
