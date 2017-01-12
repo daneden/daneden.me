bb719ec3ec844810212c42fa0f5b6c41aac4fe9f
Daniel Eden <dan.eden@me.com>
2017-01-12 14:30:15 -0800

    Update markdown renderer and options, some design tweaks

--- _plugins/typogruby.rb
+++ _plugins/typogruby.rb
@@ -2,12 +2,13 @@
 require 'typogruby'
 require 'kramdown'
 # Typogruby - better typography
-module Jekyll
-  module Converters
-    class Markdown < Converter
-      def convert(content)
-        return Typogruby.improve(Kramdown::Document.new(content).to_html)
-      end
-    end
-  end
-end
+# module Jekyll
+#   module Converters
+#     class Markdown < Converter
+#       def convert(content)
+#         # return Typogruby.improve(Kramdown::Document.new(content).to_html)
+#         return (Kramdown::Document.new(content).to_html)
+#       end
+#     end
+#   end
+# end
