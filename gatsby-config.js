197bf2b624b44992f50adec852604ffd8047ea47
Daniel Eden <dan.eden@me.com>
2018-07-10 09:01:03 -0700

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,19 @@ module.exports = {
         name: "blog",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-122108242-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+      },
+    },
   ],
 }
