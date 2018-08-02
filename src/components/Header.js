01152862e61285a636e4725454eb4d1bb95315bf
Daniel Eden <dan.eden@me.com>
2018-08-01 21:20:48 -0700

    Use gatsby-browser.js to load CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import "../fonts/fonts.css"
-import "./css/style.css"
-
 const NavLink = ({ to, label }) => (
   <Link className="plainlink" to={to}>
     {label}
