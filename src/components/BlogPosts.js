ca1f6ff508f6a7d303e47b65c4ef0669a4b6d770
Daniel Eden <dan.eden@me.com>
2018-08-01 18:51:15 -0700

    Revert GraphQL rearrangement

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,13 +1,27 @@
 import React from "react"
-import { StaticQuery } from "gatsby"
-
 import PostLink from "./PostLink"
-import queries from "../utils/queries"
+import { graphql, StaticQuery } from "gatsby"
 
 export default function BlogPosts() {
   return (
     <StaticQuery
-      query={queries.ALL_BLOG_POSTS}
+      query={graphql`
+        query {
+          allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  date(formatString: "dddd, MMMM Do YYYY")
+                }
+                fields {
+                  slug
+                }
+              }
+            }
+          }
+        }
+      `}
       render={data => (
         <ul className="unlist">
           {data.allMdx.edges.map(edge => (
