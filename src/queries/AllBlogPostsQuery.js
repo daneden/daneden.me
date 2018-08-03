237c9a8acd94ca7940cb1cff96b1fe202f8a13ee
Daniel Eden <dan.eden@me.com>
2018-08-02 18:20:58 -0700

    Isolate GraphQL queries to clean up component code

---
+++ src/queries/AllBlogPostsQuery.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+
+const AllBlogPostsQuery = ({ render }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+          edges {
+            node {
+              frontmatter {
+                title
+                date(formatString: "dddd, MMMM Do YYYY")
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => render(data)}
+  />
+)
+
+export default AllBlogPostsQuery
