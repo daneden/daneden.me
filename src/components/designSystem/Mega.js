c3e325c9a1a66f320a76b3acbaa64931dc2be065
Daniel Eden <dan.eden@me.com>
2018-09-25 07:51:17 -0700

    Reduce font weight for `mega` component

--- src/components/designSystem/Mega.js
+++ src/components/designSystem/Mega.js
@@ -5,6 +5,7 @@ import Atoms from "./atoms"
 export default styled("h1")`
   font-family: ${Atoms.font.family.sans};
   font-size: 6vw;
+  font-weight: 400;
   letter-spacing: -0.015em;
   line-height: 1;
   margin-bottom: ${Atoms.spacing.large};
