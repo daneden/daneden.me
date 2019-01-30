b82b8b03c6cec44e8c857077232a3c3d77453653
Daniel Eden <dan.eden@me.com>
2019-01-30 15:29:08 -0800

    Fix import statement

--- src/components/designSystem/Sans.js
+++ src/components/designSystem/Sans.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled"
 
-import { Atoms } from "./designSystem"
+import Atoms from "./atoms"
 
 export default styled("span")`
   font-family: ${Atoms.font.family.sans};
