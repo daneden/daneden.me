e2f605ec7846034741ae9f4358a3574974bb4a48
Daniel Eden <dan.eden@me.com>
2019-03-16 23:28:39 -0700

    Initial redesign commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -30,7 +30,6 @@ export default function Layout({ children, location, ...props }) {
                 margin: 0,
                 padding: 0,
               },
-
               html: {
                 backgroundColor: Atoms.colors.wash,
                 color: Atoms.colors.text,
