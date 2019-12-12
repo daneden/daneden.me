f9f3b3cd3e159a8e94826b840262991b87826277
Daniel Eden <dan.eden@me.com>
2019-12-12 13:44:46 +0000

    Fix 404 page graphic

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -1,38 +1,16 @@
-import { keyframes } from '@emotion/core'
 import styled from '@emotion/styled'
 import React, { ReactElement } from 'react'
-import Atoms from './designSystem/atoms'
-import H1 from './designSystem/H1'
+import Breakout from './Breakout'
 
-const { pnf, fof } = {
-  pnf: 8.45,
-  fof: 1.3562091503,
-}
-
-const bgMarquee = keyframes`
-  from {
-    background-position: ${pnf * -10}vmin 0, ${fof * 50}vmin 10vmin, ${pnf *
-  -5}vmin 60vmin;
-  }
-
-  to {
-    background-position: ${pnf * 10}vmin 0, ${fof * -50}vmin 10vmin, ${pnf *
-  5}vmin 60vmin;
-  }
-`
-
-const StyledH1 = styled(H1)`
+const StyledH1 = styled.h1`
   height: 70vmin;
   margin: 0;
-  margin-left: -50vw;
   padding: 0;
-  width: 100vw;
   background-image: url('/images/pnf.svg'), url('/images/404.svg'),
     url('/images/pnf.svg');
   background-repeat: repeat-x;
   background-size: auto 10vmin, auto 50vmin, auto 10vmin;
-  background-position: 0 0, 0 10vmin, 0 60vmin;
-  animation: ${bgMarquee} 10s infinite linear;
+  background-position: center 0, center 10vmin, center 60vmin;
   span {
     visibility: hidden;
   }
@@ -40,18 +18,10 @@ const StyledH1 = styled(H1)`
 
 export default function FourOhFour(): ReactElement<HTMLDivElement> {
   return (
-    <div
-      style={{
-        marginBottom: Atoms.spacing.large,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        textAlign: 'center',
-        width: '0',
-      }}
-    >
+    <Breakout>
       <StyledH1>
         <span>404 Page Not Found</span>
       </StyledH1>
-    </div>
+    </Breakout>
   )
 }
