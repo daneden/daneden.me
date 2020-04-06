f50253a556255ccb05cab8656fd870e2c3d51eee
Daniel Eden <dan.eden@me.com>
2020-04-06 14:46:36 +0100

    Publish “3 Strategies for Effective Cross-Team Collaboration”

--- src/components/designSystem/Mark.tsx
+++ src/components/designSystem/Mark.tsx
@@ -5,6 +5,8 @@ export default styled('mark')`
   background-color: ${Atoms.colors.mark};
   border-radius: ${Atoms.spacing.xxsmall};
   color: inherit;
+  margin-left: -${Atoms.spacing.xxsmall};
+  margin-right: -${Atoms.spacing.xxsmall};
   padding-left: ${Atoms.spacing.xxsmall};
   padding-right: ${Atoms.spacing.xxsmall};
 `
