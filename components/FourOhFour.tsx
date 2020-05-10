8db9b23f34583fd2ad22ffd4a79fc6f7923f96a5
Daniel Eden <dan.eden@me.com>
2020-05-10 17:17:39 +0100

    Fix 404 images

--- components/FourOhFour.tsx
+++ components/FourOhFour.tsx
@@ -8,7 +8,7 @@ const StyledH1 = styled.h1`
   height: 70vmin;
   margin: 0;
   padding: 0;
-  background-image: url("/images/pnf.svg"), url("/images/404.svg"),
+  background-image: url("/static/images/pnf.svg"), url("/static/images/404.svg"),
     url("/images/pnf.svg");
   background-repeat: repeat-x;
   background-size: auto 10vmin, auto 50vmin, auto 10vmin;
