a3d87f2b503b49c413f0040a99680a519ed0eedf
Daniel Eden <dan.eden@me.com>
2020-05-11 13:35:40 +0100

    Remove logging for analytics

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -6,11 +6,9 @@ declare global {
   }
 }
 export const initGA = (): void => {
-  console.log("GA init")
   ReactGA.initialize("UA-122108242-1")
 }
 export const logPageView = (): void => {
-  console.log(`Logging pageview for ${window.location.pathname}`)
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
