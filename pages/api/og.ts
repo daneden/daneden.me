be9ea4333949a5478b40df8bc02e40a1a74443fb
Daniel Eden <dan.eden@me.com>
2020-06-07 16:56:57 +0100

    Create dedicated page for opengraph images

--- pages/api/og.ts
+++ pages/api/og.ts
@@ -1,78 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { NowRequest, NowResponse } from "@now/node"
 import chrome from "chrome-aws-lambda"
-import fs from "fs"
-import path from "path"
 import puppeteer from "puppeteer-core"
-import { Atoms } from "@/designSystem"
 
-const generateHTML = (title = "Hello world") => {
-  return `<html>
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-      }
-
-      html {
-        width: 1200px;
-        height: 1200px;
-        background-color: ${Atoms.colors.text};
-      }
-
-      body {
-        position: relative;
-      }
-
-      @font-face {
-        font-family: 'Soehne Breit Web';
-        font-style: normal;
-        font-weight: bold;
-        src: url('/fonts/soehne-breit-web-fett.woff2') format('woff2');
-      }
-
-      @font-face {
-        font-family: 'National 2 Web';
-        font-style: normal;
-        src: url('/fonts/National2Web-Regular.woff2') format('woff2');
-      }
-
-      .title {
-        font: 96px/1 "Soehne Breit Web", sans-serif;
-        color: ${Atoms.colors.wash};
-        max-width: 1024px;
-      }
-
-      .title p {
-        position: absolute;
-        top: 50%;
-        left: 66px;
-        transform: translateY(-55%);
-        margin: 0;
-      }
-
-      .author {
-        font: 36px/1 "National 2", sans-serif;
-        color: ${Atoms.colors.whiteAlpha};
-        transform: rotate(-90deg) translateY(300%);
-        position: absolute;
-        top: 50%;
-        right: 0;
-      }
-    </style>
-
-    <div class="title" style="position: relative; height: 100%; width: 100%;">
-      <p>${title.replace(/ ([^ ]*)$/, "\u00A0$1")}</p>
-    </div>
-
-    <p class="author">Daniel Eden, Designer</p>
-
-  </html>
-  `
-}
-
-const getScreenshot = async function ({ html, type = "png" }) {
-  if (!html) {
-    throw Error("You must provide an html property.")
+const getScreenshot = async function ({ url, type = "png" }) {
+  if (!url) {
+    throw Error("You must provide a URL.")
   }
 
   const browser = await puppeteer.launch({
@@ -98,7 +31,7 @@ const getScreenshot = async function ({ html, type = "png" }) {
   console.log(fontFaceObserver.substring(0, 20))
 
   const page = await browser.newPage()
-  await page.setContent(html, {
+  await page.goto(url, {
     waitUntil: "networkidle2",
   })
   await page.addScriptTag({
@@ -121,8 +54,9 @@ export default async (request: NowRequest, response: NowResponse) => {
     response.status(404).end()
   }
 
-  const html = generateHTML(String(title))
-  const result = await getScreenshot({ html })
+  const result = await getScreenshot({
+    url: `https://${process.env.VERCEL_URL}/og?title=${title}`,
+  })
   response.writeHead(200, { "Content-Type": "image/png" })
   response.end(result)
   return
---
+++ pages/og.tsx
@@ -0,0 +1,94 @@
+import { Atoms } from "@/designSystem"
+import { css, Global } from "@emotion/core"
+import FontFaceObserver from "fontfaceobserver"
+import { GetServerSideProps } from "next"
+import React, { ReactFragment } from "react"
+import { Helmet } from "react-helmet"
+
+const { useEffect } = React
+
+declare global {
+  interface Window {
+    FontFaceObserver: unknown
+  }
+}
+
+export default function OpenGraphImagePage({
+  title = "Hello World",
+}): ReactFragment {
+  useEffect(() => {
+    window.FontFaceObserver = FontFaceObserver
+  })
+
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+          }
+
+          html,
+          #__next {
+            width: 1200px;
+            height: 1200px;
+            background-color: ${Atoms.colors.text};
+          }
+
+          body {
+            position: relative;
+          }
+
+          .title {
+            font: 96px/1 "Soehne Breit Web", sans-serif;
+            color: ${Atoms.colors.wash};
+            max-width: 1024px;
+          }
+
+          .title p {
+            position: absolute;
+            top: 50%;
+            left: 66px;
+            transform: translateY(-55%);
+            margin: 0;
+          }
+
+          .author {
+            font: 36px/1 "National 2 Web", sans-serif;
+            color: ${Atoms.colors.whiteAlpha};
+            transform: rotate(-90deg) translateY(300%);
+            position: absolute;
+            top: 50%;
+            right: 0;
+          }
+        `}
+      />
+
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+
+      <div
+        className="title"
+        style={{ position: "relative", height: "100%", width: "100%" }}
+      >
+        <p>{title.replace(/ ([^ ]*)$/, "\u00A0$1")}</p>
+      </div>
+
+      <p className="author">Daniel Eden, Designer</p>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {
+    query: { title = "Hello World" },
+  } = context
+
+  return {
+    props: {
+      title,
+    },
+  }
+}
