00e735aeac02a9fcde764fff0a6b5204df42d633
Daniel Eden <dan.eden@me.com>
2020-06-09 16:06:36 +0100

    Try to fix caching and remove unused const from index layout

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -2,10 +2,6 @@ import Layout from "components/Layout"
 import { ReactElement } from "react"
 export default function DefaultLayout(frontMatter) {
   return ({ children }): ReactElement<typeof Layout> => {
-    const slug = frontMatter.__resourcePath
-      .replace("blog/", "")
-      .replace(".mdx", "")
-
     return <Layout frontMatter={frontMatter}>{children}</Layout>
   }
 }
