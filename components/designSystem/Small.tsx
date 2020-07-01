5850e754518d014248c71259f4701042295940dc
Daniel Eden <dan.eden@me.com>
2020-07-01 13:55:24 +0100

    Fix TS error

--- components/designSystem/Small.tsx
+++ components/designSystem/Small.tsx
@@ -1,16 +1,18 @@
 import styled from "@emotion/styled"
 import { Atoms } from "."
 
+type DisplayType = "block" | "inline-block"
+
 interface Props {
   receded?: boolean
-  display: "block" | "inline-block"
+  display?: DisplayType
   as?: React.ElementType
 }
 export default styled.small<Props>`
   font-size: ${Atoms.font.size.small};
   letter-spacing: 0.025em;
   line-height: ${Atoms.baseline};
-  display: ${({ display = "inline-block" }): string => display};
+  display: ${({ display = "inline-block" }): DisplayType => display};
   color: ${({ receded = true }): string =>
     receded ? "var(--meta-color)" : "inherit"};
 `
