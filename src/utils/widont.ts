65cf4b7ec34629fefaf72a72cfa7440403bf1bd7
Daniel Eden <dan.eden@me.com>
2020-08-29 11:19:47 +0100

    Allow widows on particularly long last word pairs

--- src/utils/widont.ts
+++ src/utils/widont.ts
@@ -1,3 +1,10 @@
 export default function widont(subject: string): string {
-  return subject.replace(/ ([^ ]*)$/, "\u00A0$1")
+  const words = subject.split(" ")
+  const lastTwo = words.slice(-2).join(" ")
+
+  if (lastTwo.length >= 15) {
+    return subject
+  } else {
+    return subject.replace(/ ([^ ]*)$/, "\u00A0$1")
+  }
 }
