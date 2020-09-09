ad1b0ecb4d909516d53cc4223c693608e4d864fc
Daniel Eden <dan.eden@me.com>
2020-09-09 15:30:38 +0100

    Update site icon

--- src/components/Metatags.tsx
+++ src/components/Metatags.tsx
@@ -22,35 +22,35 @@ function Metatags(props: MetatagsProps): ReactElement<typeof Head> {
 
   return (
     <Head>
+      {/* Title */}
       <title>{pageTitle}</title>
       <meta content={pageTitle} name="title" />
+      <meta content={pageTitle} name="twitter:title" />
 
+      {/* Description */}
       <meta content={description} name="description" />
+      <meta content={description} name="twitter:description" />
+      <meta content={description} property="og:description" />
 
       {pathname && <meta content={url + pathname} property="og:url" />}
 
-      {thumbnail && <meta content={thumbnail} property="og:image" />}
-
+      {/* Thumbnail */}
       {thumbnail && (
-        <meta content={thumbnail} property=" og:image:secure_url" />
+        <>
+          <meta content={thumbnail} property="og:image" />
+          <meta content={thumbnail} property=" og:image:secure_url" />
+          <meta content={thumbnail} name="twitter:image" />
+        </>
       )}
-
-      <meta content={description} property="og:description" />
-
       <meta content="1200" property="og:image:width" />
-
       <meta content="630" property="og:image:height" />
+      <meta content="summary_large_image" name="twitter:card" />
 
       <meta content="en" property="og:locale" />
 
-      <meta content={pageTitle} name="twitter:title" />
-
-      <meta content={description} name="twitter:description" />
-      <meta content="summary_large_image" name="twitter:card" />
-
-      {thumbnail && <meta content={thumbnail} name="twitter:image" />}
-
-      <link href="/images/favicon.png" rel="shortcut icon" />
+      {/* Favicon */}
+      <link href="/images/favicon.svg" rel="icon" type="image/svg+xml" />
+      <link color="#888" href="/images/favicon.svg" rel="mask-icon"></link>
 
       {dnsPrefetchURLs.map((url, index) => [
         <link href={url} key={`preconnect-${index}`} rel="preconnect" />,
