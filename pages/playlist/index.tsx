7e856f92b1e1a213a6978369e4845989cacbf0d8
Daniel Eden <dan.eden@me.com>
2020-12-16 17:45:16 +0000

    Remove music api stuff

--- pages/playlist/index.tsx
+++ pages/playlist/index.tsx
@@ -1,11 +1,9 @@
 import BookList from "@/components/BookList"
 import Layout from "@/components/Layout"
-import RecentlyPlayed from "@/components/RecentlyPlayed"
 
 export default function LibraryPage() {
   return (
     <Layout frontMatter={{ title: "Playlist" }}>
-      <RecentlyPlayed />
       <BookList />
     </Layout>
   )
