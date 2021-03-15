fdd8605085c36fdb58239d720a2b97abb116a5d1
Daniel Eden <dan.eden@me.com>
2021-03-15 14:11:58 +0000

    Use getStaticProps for playlist page

--- pages/playlist/index.tsx
+++ pages/playlist/index.tsx
@@ -1,10 +1,64 @@
-import BookList from "@/components/BookList"
 import Layout from "@/components/Layout"
+import Atoms from "@/designSystem/atoms"
+import { GetStaticProps } from "next"
+import bookDataSource from "@/data/books.json"
+import Book, { BookData } from "@/components/Book"
+import widont from "@/utils/widont"
+import React from "react"
+import Breakout from "@/components/Breakout"
 
-export default function LibraryPage() {
+// This function is used to help sort titles excluding leading "the/a"
+const strippedTitle = (str: string): string =>
+  str.replace(/^(the|a) /i, "").toLowerCase()
+
+export default function LibraryPage({ entries }: { entries: BookData[] }) {
   return (
     <Layout frontMatter={{ title: "Playlist" }}>
-      <BookList />
+      <Breakout>
+        <div className="library">
+          {entries.map(({ title, author, quote, cover, url, type }) => (
+            <Book
+              author={author}
+              cover={cover}
+              key={title}
+              quote={quote}
+              title={title}
+              type={type}
+              url={url}
+            />
+          ))}
+        </div>
+      </Breakout>
+      <style jsx>{`
+        .library {
+          align-content: start;
+          place-items: start;
+          display: grid;
+          grid-gap: ${Atoms.spacing.large};
+          grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+          justify-items: stretch;
+        }
+      `}</style>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const entries = bookDataSource
+    .map((book) => {
+      return {
+        ...book,
+        // Replace the last space with a non-breaking space
+        title: widont(book.title),
+      }
+    })
+    .sort((a, b) =>
+      strippedTitle(a.title).localeCompare(strippedTitle(b.title))
+    ) as BookData[]
+
+  return {
+    props: {
+      entries,
+    },
+  }
 }
