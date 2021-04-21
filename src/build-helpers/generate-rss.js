7cd9a6cfee1b96b9b4666efdb7f1aafcba335247
Daniel Eden <dan.eden@me.com>
2021-04-21 13:34:11 +0100

    Add content to RSS feed

--- src/build-helpers/generate-rss.js
+++ src/build-helpers/generate-rss.js
@@ -3,6 +3,41 @@ const fs = require("fs")
 const read = require("fs-readdir-recursive")
 const matter = require("gray-matter")
 const path = require("path")
+const renderToString = require("next-mdx-remote/render-to-string")
+const katex = require("rehype-katex")
+const slug = require("rehype-slug")
+const abbr = require("remark-abbr")
+const math = require("remark-math")
+const toc = require("remark-toc")
+const NextImage = require("next/image")
+const prism = require("mdx-prism")
+const smartypants = require("@ngsctt/remark-smartypants")
+const React = require("react")
+
+const emptyFunction = () => null
+
+const Image = ({ alt, src, width, height }) => {
+  const isRemote = src.startsWith("http")
+  return React.createElement(
+    "img",
+    {
+      alt,
+      src: isRemote ? src : `https://daneden.me/uploads/${src}`,
+      width,
+      height,
+    },
+    null
+  )
+}
+
+const Link = ({ href, children }) => {
+  return React.createElement(
+    "a",
+    { href: `${href.startsWith("/") ? "https://daneden.me" : ""} ${href}` },
+    children
+  )
+}
+
 const siteConfig = require(path.resolve(
   process.cwd(),
   "src",
@@ -48,17 +83,50 @@ const postsMap = new Map(
     })
 )
 
-postsMap.forEach(function addRssItem(post, path) {
-  feed.item({
-    title: post.frontMatter.title,
-    url: `https://daneden.me/blog/${path}`,
-    guid: path,
-    categories: post.frontMatter.categories,
-    date: post.frontMatter.date,
+let promises = []
+let posts = []
+
+postsMap.forEach(async function addRssItem(post, path) {
+  const { content, frontMatter } = post
+  const components = {
+    Image,
+    a: Link,
+    Video: emptyFunction,
+    TypedSystemsButton: emptyFunction,
+    Codepen: emptyFunction,
+  }
+
+  const mdxPromise = renderToString(content, {
+    components,
+    mdxOptions: {
+      remarkPlugins: [abbr, smartypants, math, toc],
+      rehypePlugins: [katex, prism, slug],
+    },
   })
+
+  promises.push(mdxPromise)
+  posts.push({ ...post.frontMatter, path })
 })
 
-const xmlString = feed.xml()
-const filePath = path.resolve(process.cwd(), "public", "feed.xml")
+Promise.all(promises)
+  .then((mdxSources) => {
+    mdxSources.map((mdxSource, i) => {
+      const post = posts[i]
+      feed.item({
+        title: post.title,
+        url: `https://daneden.me/blog/${post.path}`,
+        guid: post.path,
+        categories: post.categories,
+        date: post.date,
+        description: mdxSource.renderedOutput,
+      })
+    })
 
-fs.writeFileSync(filePath, xmlString)
+    return
+  })
+  .then(() => {
+    const xmlString = feed.xml()
+    const filePath = path.resolve(process.cwd(), "public", "feed.xml")
+
+    fs.writeFileSync(filePath, xmlString)
+  })
