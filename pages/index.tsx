499d68f5104858d72f6564aea2d65d011c1e56db
Daniel Eden <dan.eden@me.com>
2022-01-03 21:53:45 +0000

    Update home page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,127 +1,37 @@
-import FeatureTile from "@/components/FeatureTile"
-import Image from "@/components/Image"
+import Breakout from "@/components/Breakout"
 import Layout from "@/components/Layout"
+import Timeline from "@/components/Timeline"
 import Link from "next/link"
 
 export default function HomePage() {
   return (
     <Layout>
-      <div className="image-container">
-        <span className="badge" aria-hidden={true}>
-          Employed
-        </span>
-        <span className="badge" aria-hidden={true}>
-          British
-        </span>
-        <span className="badge" aria-hidden={true}>
-          “Unicorn”
-        </span>
-        <Image
-          alt="Daniel Eden"
-          height={200}
-          src="2020/11/face.jpg"
-          width={200}
-        />
-      </div>
-      <p className="intro">
-        Daniel Eden is a Product Designer at{" "}
-        <Link href="https://meta.com/">Meta</Link> in London, working on making
-        Customer Support experiences that are more equitable, human, and
-        helpful. He spends his time <Link href="/blog">writing</Link>, thinking,{" "}
-        <Link href="https://twitter.com/_dte">tweeting</Link>, and talking about
-        Design Systems: how they scale, how they break, and the people&nbsp;that
-        maintain&nbsp;them.
-      </p>
-      <p>
-        You probably came here to <Link href="/blog">read the blog</Link> or see{" "}
-        <Link href="/portfolio">my work</Link>.
-      </p>
-      <hr />
-      <h2>Selected Work and Writing</h2>
-      <FeatureTile
-        url="/blog/2019/where-we-can-go"
-        title="Where We Can Go"
-        description="Today’s design and engineering tools are poorly suited for creating or working within design systems. What would our tools look like if they treated design systems as the building material, rather than an abstract thing we gesture towards?"
-      />
-      <FeatureTile
-        url="https://solstice.daneden.me"
-        title="Solstice"
-        description="An app for brighter days. See how much more or less daylight there is today compared to yesterday."
-        image={
-          <Image
-            src="2021/05/solstice-app-icon.png"
-            width={512}
-            height={512}
-            alt="The app icon for Solstice"
-            sizes={"4rem"}
-          />
-        }
-      />
-      <FeatureTile
-        url="https://breadforthebusy.com"
-        title="Bread For The Busy"
-        description="A website about making bread, geared towards no-nonsense recipes."
-        image={
-          <Image
-            src="2021/05/perfect-sandwich-loaf.jpg"
-            width={4000}
-            height={6000}
-            alt="A loaf of sourdough bread"
-            sizes={"4rem"}
-          />
-        }
-      />
-      <FeatureTile
-        url="/blog/2020/access-ability"
-        title="Access—Ability"
-        description="People tend to conflate “accessibility” with “disability”; but designing
-    for accessibility is principally a question of “access”. Framed in this
-    light, the challenges and opportunities of accessible design become more
-    vividly obvious."
-      />
+      <Breakout>
+        <p className="intro">
+          Daniel Eden is a Product Designer at{" "}
+          <Link href="https://meta.com/">Meta</Link> in London, working on
+          making Customer Support experiences that are more equitable, human,
+          and helpful. He spends his time <Link href="/blog">writing</Link>,
+          thinking, <Link href="https://twitter.com/_dte">tweeting</Link>, and
+          talking about Design Systems: how they scale, how they break, and the
+          people&nbsp;that maintain&nbsp;them.
+        </p>
+        <Timeline />
+      </Breakout>
       <style jsx>{`
         .intro {
-          z-index: 2;
+          font-size: clamp(1.5rem, 12vmax, 3.5rem);
+          line-height: 1.2;
+          font-style: normal;
+          font-family: var(--font-sans);
+          letter-spacing: -0.025em;
         }
 
-        .image-container {
-          position: relative;
-          padding-block: var(--sp-l);
-        }
-
-        .badge {
-          --hue: 70;
-          font-family: var(--font-sans-extended);
-          padding: 1rem 1.5rem;
-          border-radius: 100%;
-          background-color: var(--site-color);
-          background-color: lch(60% 180 var(--hue));
-          color: var(--wash-color);
-          position: absolute;
-          text-transform: uppercase;
-          z-index: 2;
-          font-size: 1.75rem;
-          letter-spacing: 0.05em;
-        }
-
-        .badge:nth-of-type(1) {
-          top: 2.5%;
-          left: -2.5%;
-          transform: rotate(-5deg);
-        }
-
-        .badge:nth-of-type(2) {
-          right: calc(var(--sp-xs) * -1);
-          top: 25%;
-          transform: rotate(5deg);
-          --hue: 0;
-        }
-
-        .badge:nth-of-type(3) {
-          left: 5%;
-          bottom: 2.5%;
-          transform: rotate(7deg);
-          --hue: 280;
+        .intro :global(a) {
+          --padding-size: 0.05em;
+          font-family: var(--font-serif);
+          font-style: italic;
+          text-decoration-thickness: 0.05em;
         }
       `}</style>
     </Layout>
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,86 @@
+import timeline from "@/data/timeline.json"
+import Link from "next/link"
+
+interface TimelineEntries {
+  [key: string]: [
+    {
+      date: Date
+      url: string
+      title: string
+      description: string
+    }
+  ]
+}
+
+export default function Timeline() {
+  const orderedData = timeline
+    .map((entry) => ({ ...entry, date: new Date(entry.date) }))
+    .sort((a, b) => a.date.valueOf() - b.date.valueOf())
+    .reduce<TimelineEntries>((sections, currentValue) => {
+      const year = currentValue.date.getFullYear().toString()
+      if (sections[year] != undefined) {
+        sections[year].push(currentValue)
+      } else {
+        sections[year] = [currentValue]
+      }
+
+      return sections
+    }, {})
+
+  return (
+    <>
+      <table>
+        <tbody>
+          {Object.keys(orderedData)
+            .sort()
+            .reverse()
+            .map((year) => {
+              return orderedData[year].map((entry, index) => (
+                <tr key={year}>
+                  {index === 0 ? <th>{year}</th> : <td />}
+                  <td>
+                    <Link href={entry.url}>{entry.title}</Link>
+
+                    <p className="small sans meta">{entry.description}</p>
+                  </td>
+                </tr>
+              ))
+            })}
+        </tbody>
+      </table>
+      <style jsx>{`
+        table {
+          font: inherit;
+          width: 100%;
+        }
+
+        th {
+          font-weight: normal;
+          border: none;
+          font-size: inherit;
+          text-align: right;
+          width: calc(
+            ((100vw - (var(--sp-m) * 2)) - var(--container-width)) / 2
+          ) !important;
+          color: var(--meta-color);
+        }
+
+        th,
+        td {
+          font-family: var(--font-sans);
+          border: none;
+          line-height: 1.2;
+        }
+
+        th,
+        th + td {
+          border-top: 2px solid var(--meta-color) !important;
+        }
+
+        table :global(a) {
+          text-decoration: none;
+        }
+      `}</style>
+    </>
+  )
+}
