6349f8645430a0a468ee98b30f3d2b5bf64d0914
Daniel Eden <dan.eden@me.com>
2022-02-06 13:01:07 +0000

    Try to fix node-canvas builds

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`
+}
+
 module.exports = {
   swcMinify: true,
   env: {
