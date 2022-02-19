8b0c6118cf7a97141f09c804fa18e93677520231
Daniel Eden <dan.eden@me.com>
2022-02-19 11:42:59 +0000

    Remove unused config options

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`
-}
-
 module.exports = {
-  experimental: { esmExternals: true },
   swcMinify: true,
   env: {
     VERCEL_URL: process.env.VERCEL_URL || "daneden.me",
