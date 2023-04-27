5da905a424890a3f7c1ab83c8b0ab91059aef405
Daniel Eden <dan.eden@me.com>
2023-04-27 15:24:40 +0100

    Try to use absolute remote font fetching to handle og fonts

--- src/utils/opengraphImage.tsx
+++ src/utils/opengraphImage.tsx
@@ -3,11 +3,14 @@ import widont from "@/utils/widont"
 import { ImageResponse } from "next/server"
 
 const tiemposHeadline = fetch(
-  new URL("../../app/fonts/ogFonts/TiemposHeadline-Light.otf", import.meta.url)
+  new URL(
+    "https://daneden.me/og-fonts/TiemposHeadline-Light.otf",
+    import.meta.url
+  )
 ).then((res) => res.arrayBuffer())
 
 const soehne = fetch(
-  new URL("../../app/fonts/ogFonts/Soehne-Buch.otf", import.meta.url)
+  new URL("https://daneden.me/og-fonts/Soehne-Buch.otf", import.meta.url)
 ).then((res) => res.arrayBuffer())
 
 export default async function opengraphImage(title = config.title) {
