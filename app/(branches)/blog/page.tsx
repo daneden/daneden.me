1baec9e639bb8c1691c31399247a804fdbdfec2c
Daniel Eden <dan.eden@me.com>
2023-04-29 09:23:43 +0100

    Update blog list to use Edge runtime

--- app/(branches)/blog/page.tsx
+++ app/(branches)/blog/page.tsx
@@ -2,6 +2,8 @@ import formatDate from "@/utils/formatDate"
 import { allPosts } from "contentlayer/generated"
 import Link from "next/link"
 
+export const runtime = "edge"
+
 export const metadata = {
   title: "Blog",
   description: "Daniel Eden’s Blog",
