75c7e257a76334c0c33d5fda280c6b65435ba82a
Daniel Eden <dan.eden@me.com>
2023-04-29 10:15:17 +0100

    Remove edge runtime directive since contentlayer output is just above the 1mb limit 😓

--- app/(branches)/blog/[...slug]/page.tsx
+++ app/(branches)/blog/[...slug]/page.tsx
@@ -5,8 +5,6 @@ import { notFound } from "next/navigation"
 import PostContent from "./PostContent"
 import styles from "./styles.module.css"
 
-export const runtime = "edge"
-
 export async function generateMetadata({
   params: { slug },
 }: {
