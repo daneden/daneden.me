8a3164393d7c13b1bdee14024f573b7971368244
Daniel Eden <dan.eden@me.com>
2023-04-30 17:47:32 +0100

    Actually await og image function

--- app/api/opengraph-image/route.ts
+++ app/api/opengraph-image/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const title = searchParams.get("title")
 
-  return opengraphImage(title ?? undefined)
+  return await opengraphImage(title ?? undefined)
 }
 
 export const runtime = "edge"
