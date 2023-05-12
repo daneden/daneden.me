18ba275c07985cd38320e650267ba8383ffeca7a
Daniel Eden <dan.eden@me.com>
2023-05-12 15:12:35 +0100

    Lots of changes

--- app/(branches)/portfolio/solstice/SolsticeTitle/index.tsx
+++ app/(branches)/portfolio/solstice/SolsticeTitle/index.tsx
@@ -3,28 +3,24 @@ import styles from "./styles.module.css"
 export default function SolsticeTitle() {
   return (
     <header className={styles.root}>
-      <h1 className={styles.title}>Solstice</h1>
-      <div className={styles.solarChart}>
-        <div className={styles.horizonUpper} />
+      <div className={styles.iconContainer}>
         <svg
-          className={styles.sineWave}
-          width={400}
-          height={200}
-          viewBox="50 -4 200 200"
+          className={styles.icon}
+          viewBox="0 0 373 363"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M 0 100 Q 50 200 100 100 Q 150 0 200 100 Q 250 200 300 100 "
+            d="M186.609 346.591V315.338M82.1088 325.941L97.7349 298.876M284.272 325.941L268.646 298.876M66.9709 141.009C66.9709 207.083 120.535 260.647 186.609 260.647C252.682 260.647 306.246 207.083 306.246 141.009M16 255.872L43.2532 240.137M356.626 255.676L329.712 240.137M114.338 88.7592C114.338 88.7592 151.943 45.8475 186.609 16M186.609 16C220.337 45.8475 256.926 88.7592 256.926 88.7592M186.609 16V141.009"
             stroke="currentcolor"
-            strokeLinecap="round"
-            strokeWidth="4"
-            fill="none"
-            opacity="0.2"
+            stroke-width="31.2523"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           />
-          <circle cx="150" cy="50" r="10" fill="white" />
         </svg>
-        <div className={styles.horizonLine} />
-        <div className={styles.horizonLower} />
       </div>
+      <span className={styles.tagline}>Deep Dive</span>
+      <h1 className={styles.title}>Solstice</h1>
     </header>
   )
 }
