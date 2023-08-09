6c31889c1864db11196fe5789ef8d93419c0bb6f
Daniel Eden <dan.eden@me.com>
2023-08-09 22:07:28 +0100

    Update index.tsx

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -7,11 +7,8 @@ export default function Footer() {
       <footer className={styles.root}>
         <div className={`${styles.wrapper} small`}>
           Written, designed, and built by Daniel Eden, a designer who you can
-          find on <Link href="https://twitter.com/_dte">Twitter</Link>,{" "}
-          <Link rel="me" href="https://mastodon.online/@dte">
-            Mastodon
-          </Link>
-          , <Link href="https://github.com/daneden">GitHub</Link>, or good
+          find on <Link href="https://threads.net/@_dte?rel=me">Threads</Link>,
+          <Link href="https://github.com/daneden?rel=me">GitHub</Link>, or good
           old-fashioned <Link href="mailto:dan.eden@me.com">Email</Link>.
         </div>
       </footer>
