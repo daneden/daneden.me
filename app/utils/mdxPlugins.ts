2a795a95e3b48b04dc4c8fb6d3980b99b02843f9
Dan Eden <dan.eden@me.com>
2026-01-09 12:08:48 +0000

    Migrate from yarn to bun

---
+++ app/utils/mdxPlugins.ts
@@ -0,0 +1,8 @@
+export const remarkPlugins = [
+  'remark-gfm',
+  '@ngsctt/remark-smartypants',
+  'remark-math',
+  'remark-toc',
+  'remark-directive',
+]
+export const rehypePlugins = ['rehype-katex', 'rehype-prism-plus']
